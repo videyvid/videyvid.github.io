67fd4cb0df9a7ec83f945ed274f23a599e9a84ff
videyvid <116954440+videyvid@users.noreply.github.com>
2025-06-16 23:47:24 +0700

    Delete alert.js

--- alert.js
+++
@@ -1,3 +0,0 @@
-<script>
-alert('Are You 18+ Old?'); window.open("https://googleweblight.com/sp?u=https://www.profitablegatetocontent.com/ri2aefi1p8?key=a10b110330839bbfbb7fec351a7ec05d");
-</script>
